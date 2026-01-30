360326a7ff5b4b3d9ea2a2270789c6b301a05822
Rayhan three <rayhanthreedamara1098@gmail.com>
2026-01-30 16:28:29 +0700

    Update ControlApps.js

--- database/ControlApps.js
+++ database/ControlApps.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tokens: "8308158315:AAFLlkqPnWvmEBzq11i1sZ7n3fTnaHMfM00", 
-  owner: "1011991187", 
+  tokens: "7702637730:AAF458gmRiDK5RZTEQyD3aNObny4zxUXpmQ", 
+  owner: "7988464871", 
   port: "2000", // Ini Wajib Jangan Diubah
   ipvps: "https://newparadoxpanel.boosparadok.tech" // Jangan Diubah Nanti Eror!!
 };
