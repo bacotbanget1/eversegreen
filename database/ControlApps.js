76ffb7fa3617b3d137571274513bc2e244730d4e
Rayhan three <rayhanthreedamara1098@gmail.com>
2026-01-30 16:20:34 +0700

    Add files via upload

---
+++ database/ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8308158315:AAFLlkqPnWvmEBzq11i1sZ7n3fTnaHMfM00", 
+  owner: "1011991187", 
+  port: "2000", // Ini Wajib Jangan Diubah
+  ipvps: "https://newparadoxpanel.boosparadok.tech" // Jangan Diubah Nanti Eror!!
+};
